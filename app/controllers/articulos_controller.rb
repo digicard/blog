fd38484b3c1687c99210876231fdd519888da0a8
Digicard <digicard@lxw7993.digicard.com.ar>
2016-03-04 12:05:42 -0300

    Primer pull del blog

---
+++ app/controllers/articulos_controller.rb
@@ -0,0 +1,27 @@
+class ArticulosController < ApplicationController
+	def index
+		@articulos = Articulo.all
+	end
+
+	def new
+	end
+
+	def show
+    	@articulo = Articulo.find(params[:id])
+  	end
+
+	def create
+		 
+		# render plain: params[:articulo].inspect
+		@articulo = Articulo.new(articulo_params)
+
+		@articulo.save
+		redirect_to @articulo
+
+	end
+
+	private
+	  def articulo_params
+	    params.require(:articulo).permit(:titulo, :texto)
+	  end
+end
